171baeb994feadf242f55e39649b6c9df9bd4a62
Sahaparp Seekaewsiw <sahaparp.se@gmail.com>
2025-07-28 10:08:45 +0700

    add rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,24 +2,22 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ConfigModule } from '@nestjs/config';
+import { ThrottlerModule } from '@nestjs/throttler';
 
 @Module({
   imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule],
-      useFactory: (config: ConfigService) => ({
-        type: 'mysql',
-        host: config.get('DB_HOST'),
-        port: +config.get('DB_PORT'),
-        username: config.get('DB_USERNAME'),
-        password: config.get('DB_PASSWORD'),
-        database: config.get('DB_DATABASE'),
-        autoLoadEntities: true,
-        synchronize: true,
-      }),
-      inject: [ConfigService],
+    ThrottlerModule.forRoot([{ ttl: 60000, limit: 10 }]), // ทำ Rate Limit ของการเข้าถึงข้อมูล
+    ConfigModule.forRoot({ isGlobal: true }), // ใส่เพื่อจะใช้งาน .env ได้ทั้งหมด
+    TypeOrmModule.forRoot({ // ตัวนี้จะเป็นตัวที่เชื่อมกับฐานข้อมูล
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT ? +process.env.DB_PORT : 3306,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+      autoLoadEntities: true,
+      synchronize: true,
     }),
   ],
   controllers: [AppController],
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,13 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(helmet()); // ตัวนี้จะเป็นตัวที่ป้องกันการลักษณะการทำงานของระบบ
+  app.enableCors(); // ตัวนี้จะเป็นตัวที่อนุญาต cors ที่จะใช้งาน
+  app.useGlobalPipes(new ValidationPipe()); // ตัวนี้จะเป็นตัวที่ตรวจสอบข้อมูลที่ส่งมา
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
